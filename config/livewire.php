8043e869a4fda0a9d9382c68810cdc7d2908ff2d
leynerVM <leynervega0413@gmail.com>
2024-09-06 19:36:23 -0500

    actualizacion arhivo config livewire

--- config/livewire.php
+++ config/livewire.php
@@ -54,8 +54,8 @@ return [
     |
     */
 
-    'asset_url' => null,
-
+    'asset_url' => env('APP_URL'),
+    'force_https' => true, // Agrega esta línea
     /*
     |--------------------------------------------------------------------------
     | Livewire App URL
